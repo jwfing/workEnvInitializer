37b17932e3beee6b62b293dc68616c581aab7d7c
jwfing <jwfing@gmail.com>
2012-06-25 19:00:53 +0800

    script/installer.sh

---
+++ script/installer.sh
@@ -0,0 +1,15 @@
+# foundation
+port
+
+# for clojure
+sudo port install clojure clojure-contrib clojure-contrib-devel clojure-devel leiningen
+
+# for vim
+
+# java
+
+# for c++
+
+# for imagemagick
+
+
